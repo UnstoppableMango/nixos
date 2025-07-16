3939fc228fdb1d5a601bb52030566d2c06ca509a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-15 22:01:21 -0500

    It compiles now I guess

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,10 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # imports =
-  #   [
-  #     ./hardware-configuration.nix
-  #   ];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
   # Enable nix flakes
   nix.package = pkgs.nixVersions.stable;
@@ -12,28 +12,43 @@
     experimental-features = nix-command flakes
   '';
 
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "hades";
-
-  time.timeZone = "America/Chicago";
-
+  networking.networkmanager.enable = true;
   networking.useDHCP = false;
   networking.interfaces.enp6s0.useDHCP = true;
   networking.interfaces.enp7s0.useDHCP = true;
 
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    # keyMap = "us";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
   # Enable the Plasma 5 Desktop Environment
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.displayManager.sddm.enable = true;
 
   # Enable CUPS
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
   # Enable sound
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
 
   # Enable touchpad support
   # services.xserver.libinput.enable = true;
@@ -42,4 +57,28 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
   };
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
