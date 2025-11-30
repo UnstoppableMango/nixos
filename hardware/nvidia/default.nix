1207ee26bc2448e30f2ef98715314b92adf1ac9b
UnstoppableMango <erik.rasmussen@unmango.dev>
2025-11-29 23:31:31 -0600

    More modules

---
+++ hardware/nvidia/default.nix
@@ -0,0 +1,13 @@
+{
+  hardware.nvidia = {
+    # Open drivers (NVreg_OpenRmEnableUnsupportedGpus=1)
+    open = true;
+
+    # nvidia-drm.modeset=1
+    modesetting.enable = true;
+
+    # Preserve video memory after suspend
+    # NVreg_PreserveVideoMemoryAllocations=1
+    powerManagement.enable = true;
+  };
+}
---
+++ users/erik/default.nix
@@ -0,0 +1,14 @@
+{ inputs, ... }:
+{
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "bak";
+  home-manager.users.erik = {
+    imports = with inputs.dotfiles.homeModules; [
+      inputs.nixvim.homeModules.nixvim
+      erik
+      gnome
+      vscode
+    ];
+  };
+}
