104395fd616393672c172f6ee4c7f114d1ccf070
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-01-07 21:28:45 -0600

    WIP

--- users/erik/default.nix
+++ users/erik/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 let
   inherit (inputs) dotfiles;
 
@@ -39,6 +39,11 @@ let
   nixos = {
     nixpkgs.overlays = [
       dotfiles.overlays.default
+    ];
+
+    users.users.erik.openssh.authorizedKeys.keys = [
+      hadesKey
+      darterKey
     ];
 
     home-manager = {
