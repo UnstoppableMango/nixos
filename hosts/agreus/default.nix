e17f790cdd389c674189a3ed637fad97c85d3f8b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-04-25 16:05:22 -0500

    factoring (#151)
    * Agreus factoring
    
    * WIP
    
    * Reset hades

--- hosts/agreus/default.nix
+++ hosts/agreus/default.nix
@@ -1,22 +1,23 @@
-{ inputs, self, ... }:
-let
-  lib = inputs.nixpkgs.lib;
-in
 {
-  flake.nixosConfigurations."agreus" = lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      {
-        imports = with inputs; [
-          disko.nixosModules.disko
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops
-          # self.modules.nixos.erik
-          self.modules.nixos.gnome
-          ./configuration.nix
-          { hardware.facter.reportPath = ./facter.json; }
-        ];
-      }
-    ];
-  };
+  disko,
+  gnome,
+  home-manager,
+  sops-nix,
+  nixosSystem,
+}:
+nixosSystem {
+  system = "x86_64-linux";
+  modules = [
+    {
+      imports = [
+        disko.nixosModules.disko
+        home-manager.nixosModules.home-manager
+        sops-nix.nixosModules.sops
+        # self.modules.nixos.erik
+        gnome
+        ./configuration.nix
+        { hardware.facter.reportPath = ./facter.json; }
+      ];
+    }
+  ];
 }
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,30 @@
+{ inputs, self, ... }:
 {
   imports = [
-    ./agreus
     ./hades
   ];
+
+  flake.nixosConfigurations = {
+    agreus = import ./agreus {
+      inherit (self.modules.nixos) gnome;
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+      inherit (inputs)
+        disko
+        home-manager
+        sops-nix
+        ;
+    };
+
+    # hades = import ./hades {
+    #   inherit (self.modules.nixos) erik gnome hades ssh nixDaemonConfig;
+    #   inherit (inputs.nixpkgs.lib) nixosSystem;
+    #   inherit (inputs)
+    #     disko
+    #     home-manager
+    #     sops-nix
+    #     ;
+    # };
+
+    # pik8s1 = import ./pik8s1;
+  };
 }
