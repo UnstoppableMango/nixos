e38fb1295129c513c747d482715183a6638eb249
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-01 23:32:26 -0500

    Moving cluster to VLAN

--- machines/pik8s4/configuration.nix
+++ machines/pik8s4/configuration.nix
@@ -5,13 +5,17 @@
 
   networking = {
     hostName = "pik8s4";
-    defaultGateway.interface = "end0";
+    defaultGateway = {
+      address = "10.0.69.1";
+      interface = "end0";
+    };
+    nameservers = [ "10.0.69.1" ];
 
     interfaces.end0 = {
       useDHCP = false;
       ipv4.addresses = [
         {
-          address = "192.168.1.104";
+          address = "10.0.69.104";
           prefixLength = 24;
         }
       ];
@@ -20,9 +24,9 @@
 
   cluster.rosequartz = {
     interface = "end0";
-    advertiseAddress = "192.168.1.104";
+    advertiseAddress = "10.0.69.104";
     keepalivedPriority = 100;
-    etcd.advertiseClientUrls = [ "https://192.168.1.104:2379" ];
-    etcd.initialAdvertisePeerUrls = [ "https://192.168.1.104:2380" ];
+    etcd.advertiseClientUrls = [ "https://10.0.69.104:2379" ];
+    etcd.initialAdvertisePeerUrls = [ "https://10.0.69.104:2380" ];
   };
 }
--- machines/pik8s6/configuration.nix
+++ machines/pik8s6/configuration.nix
@@ -5,13 +5,17 @@
 
   networking = {
     hostName = "pik8s6";
-    defaultGateway.interface = "end0";
+    defaultGateway = {
+      address = "10.0.69.1";
+      interface = "end0";
+    };
+    nameservers = [ "10.0.69.1" ];
 
     interfaces.end0 = {
       useDHCP = false;
       ipv4.addresses = [
         {
-          address = "192.168.1.106";
+          address = "10.0.69.106";
           prefixLength = 24;
         }
       ];
@@ -20,10 +24,10 @@
 
   cluster.rosequartz = {
     interface = "end0";
-    advertiseAddress = "192.168.1.106";
+    advertiseAddress = "10.0.69.106";
     keepalivedPriority = 80;
-    etcd.advertiseClientUrls = [ "https://192.168.1.106:2379" ];
-    etcd.initialAdvertisePeerUrls = [ "https://192.168.1.106:2380" ];
+    etcd.advertiseClientUrls = [ "https://10.0.69.106:2379" ];
+    etcd.initialAdvertisePeerUrls = [ "https://10.0.69.106:2380" ];
     etcd.initialClusterState = "existing";
   };
 }
