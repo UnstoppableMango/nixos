1691e7467000a0c81bf967aa4de52cfcb68068ca
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-07-01 23:32:26 -0500

    Moving cluster to VLAN

--- machines/pik8s5/configuration.nix
+++ machines/pik8s5/configuration.nix
@@ -5,13 +5,17 @@
 
   networking = {
     hostName = "pik8s5";
-    defaultGateway.interface = "end0";
+    defaultGateway = {
+      address = "10.0.69.1";
+      interface = "end0";
+    };
+    nameservers = [ "10.0.69.1" ];
 
     interfaces.end0 = {
       useDHCP = false;
       ipv4.addresses = [
         {
-          address = "192.168.1.105";
+          address = "10.0.69.105";
           prefixLength = 24;
         }
       ];
@@ -20,9 +24,9 @@
 
   cluster.rosequartz = {
     interface = "end0";
-    advertiseAddress = "192.168.1.105";
+    advertiseAddress = "10.0.69.105";
     keepalivedPriority = 90;
-    etcd.advertiseClientUrls = [ "https://192.168.1.105:2379" ];
-    etcd.initialAdvertisePeerUrls = [ "https://192.168.1.105:2380" ];
+    etcd.advertiseClientUrls = [ "https://10.0.69.105:2379" ];
+    etcd.initialAdvertisePeerUrls = [ "https://10.0.69.105:2380" ];
   };
 }
