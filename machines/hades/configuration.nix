13d389038cdb301dd6af3109247ade93431c032e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-07-20 22:59:27 -0500

    Import hades into the clan (#222)
    * Import hades into the clan
    
    * Merge hardware configuration
    
    * Remove leftover crud
    
    * ope
    
    * Explicit update
    
    * Rebase conflict

--- machines/hades/configuration.nix
+++ machines/hades/configuration.nix
@@ -33,26 +33,67 @@
   nixpkgs.config.allowUnfree = true;
 
   imports = [
-    ./hardware-configuration.nix
     ./disk-config.nix
+    ../../modules/desktops
+    ../../modules/shells
+    ../../modules/unifi
   ];
 
-  hardware.openrazer.enable = true;
-  hardware.facter.reportPath = ./facter.json;
-  hardware.facter.detected.dhcp.enable = false;
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/bcc88460-ce1a-455e-9672-4cb20c25b1bf";
+    fsType = "btrfs";
+    options = [
+      "subvol=root"
+      "compress=zstd"
+    ];
+  };
 
-  fileSystems = {
-    "/".options = [ "compress=zstd" ];
-    "/home".options = [ "compress=zstd" ];
-    "/nix".options = [
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/bcc88460-ce1a-455e-9672-4cb20c25b1bf";
+    fsType = "btrfs";
+    options = [
+      "subvol=nix"
       "compress=zstd"
       "noatime"
     ];
   };
 
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/bcc88460-ce1a-455e-9672-4cb20c25b1bf";
+    fsType = "btrfs";
+    options = [
+      "subvol=home"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/5CF7-D4A9";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  fileSystems."/var/lib/docker/btrfs" = {
+    device = "/home/root/var/lib/docker/btrfs";
+    fsType = "none";
+    options = [ "bind" ];
+  };
+
+  swapDevices = [ ];
+
+  hardware.openrazer.enable = true;
+  hardware.facter.reportPath = ./facter.json;
+  hardware.facter.detected.dhcp.enable = false;
+
   # This continues to randomly stall and fail
   # Removing just makes the problem happen later
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.kernelModules = [
+    "dm-snapshot"
+    "amdgpu"
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -224,12 +265,9 @@
   };
 
   users.users.erik = {
-    isNormalUser = true;
     shell = pkgs.zsh;
     description = "Erik Rasmussen";
     extraGroups = [
-      "networkmanager"
-      "wheel"
       "openrazer"
       "libvirt" # crc wants `libvirt` not `libvirtd`
       "docker" # for kind, hopefully
@@ -387,7 +425,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
