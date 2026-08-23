3e52f832a31f729f6cd9dc57c026645a0866475b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-08-22 20:57:13 -0500

    ssh cleanup (#265)
    * vars: update via generator openssh-cert (machine: hades)
    
    * Ssh config consolidation

---
+++ hosts.nix
@@ -0,0 +1,13 @@
+# Single source of truth for how to reach each machine.
+# Consumed by the `internet` clan service (deploys, `clan ssh`) and by hades'
+# home-manager ssh client config.
+{
+  agreus = "10.0.69.187";
+  hades = "192.168.1.69";
+  pik8s1 = "192.168.1.101";
+  pik8s2 = "192.168.1.102";
+  pik8s3 = "192.168.1.103";
+  pik8s4 = "10.0.69.104";
+  pik8s5 = "10.0.69.105";
+  pik8s6 = "10.0.69.106";
+}
