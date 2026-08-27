0122884d050b813d92b47b6dd2093f358af053d0
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-08-27 00:58:51 -0500

    network diagram

--- clan.nix
+++ clan.nix
@@ -59,17 +59,17 @@ let
     #   ];
     # };
 
-    # pollux = {
-    #   # host: inventory.instances.internet.roles.default.machines.pollux
-    #   tags = [
-    #     "basement"
-    #     "k8s"
-    #     "worker"
-    #     "rack"
-    #     "server"
-    #     "headless"
-    #   ];
-    # };
+    pollux = {
+      # host: inventory.instances.internet.roles.default.machines.pollux
+      tags = [
+        "basement"
+        "k8s"
+        "worker"
+        "rack"
+        "server"
+        "headless"
+      ];
+    };
 
     # gaea = {
     #   # host: inventory.instances.internet.roles.default.machines.gaea
@@ -168,18 +168,7 @@ in
     internet = {
       module.name = "internet";
       module.input = "clan-core";
-
-      # `hosts` covers machines this clan doesn't manage, so narrow it to the
-      # inventory before mapping: an entry here that isn't a machine fails eval.
-      roles.default.machines = builtins.mapAttrs (_: host: { settings.host = host.ip; }) (
-        builtins.intersectAttrs machines hosts
-      );
-      # pollux isn't in the dotfiles hosts.nix yet; the pinned dotfiles
-      # revision predates the table's move to the UnstoppableMango/hosts
-      # flake, so route it explicitly until this repo migrates.
-      roles.default.machines = builtins.mapAttrs (_: host: { settings.host = host; }) hosts // {
-        pollux.settings.host = "10.0.69.14";
-      };
+      roles.default.machines = machines;
     };
 
     raspberry-pi = {
